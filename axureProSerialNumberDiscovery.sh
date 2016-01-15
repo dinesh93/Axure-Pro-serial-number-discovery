98b64a7a1b817fb75f55bdcc072e3e9b95b00b8f
eholtam <eholtam@mac.com>
2016-01-14 22:56:53 -0600

    Removed extraneous grep

--- axureProSerialNumberDiscovery.sh
+++ axureProSerialNumberDiscovery.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for myuser in `ls /Users | grep -v grep | grep -v .localized | grep -v macadmin | grep -v Shared`
+for myuser in `ls /Users | grep -v .localized | grep -v macadmin | grep -v Shared`
 do
 	echo "Checking /Users/$myuser"
 	defaults read /Users/$myuser/Library/Preferences/axure.7 licensekey
